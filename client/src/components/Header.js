249b09d969b57e1db242c9ddb8e371c7f9c115cb
OLETISATISHKUMAR <satisholeti1020@gmail.com>
2023-10-19 23:53:23 +0530

    Header is updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,82 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/TravelLogo.jpg";
+
 function Header() {
   return (
-    <div>
-      <header className="flex justify-between p-4 shadow-md border border-gray-50">
-        <div className="p-4">
-          <Link to="/" className="flex gap-1 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-8 h-8 -rotate-90"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
-            <span className="font-semibold text-[18px]">Prakruthi</span>
-          </Link>
-        </div>
-        <div className="flex items-center gap-2 border border-gray-400 rounded-full py-2 px-4  shadow-md shadow-gray-300">
-          <div>Anywhere</div>
-          <div className="flex border-l border-gray-800"></div>
-          <div>Any week</div>
-          <div className="flex border-l border-gray-800"></div>
-          <div>Add guests</div>
-          <button className="bg-orange-600 text-white p-2 border rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          </button>
-        </div>
-        <Link to="/login" className="flex items-center gap-2 border border-gray-400 rounded-full py-2 px-4  shadow-md shadow-gray-300">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-8 h-8"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-          
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-8 h-8 text-gray-600"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
+    <header className="bg-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="flex items-center">
+          <img src={Logo} alt="Component Logo" className="w-20 h-20" />
+          <span className="text-black text-2xl font-semibold ml-2">
+            Prakruthi Tours and Travels
+          </span>
         </Link>
-      </header>
-    </div>
+
+        <nav className="space-x-4">
+          <Link to="/" className="text-black text-lg">Home</Link>
+          <Link to="/about" className="text-black text-lg">About</Link>
+          <Link to="/tour" className="text-black text-lg">Tour</Link>
+        </nav>
+
+        <div className="flex space-x-4 justify-start">
+          <Link to="/login" className="text-black text-lg">Login</Link>
+          <Link to="/register" className="text-black text-lg">Register</Link>
+        </div>
+      </div>
+    </header>
   );
 }
 
